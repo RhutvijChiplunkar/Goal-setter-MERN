87d9ea50f6a15e85bad742bd9b557f76122a2aa3
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2022-02-28 23:07:49 +0530

    API set up for goals of respective users

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,16 +1,19 @@
 const express=require('express');
 const router=express.Router();
 const {getGoals, setGoal, updateGoal, deleteGoal}=require('../controllers/goalController')
+const {protect}=require('../middleware/authMiddleware')
 
-//routes for /api/goals
+/*routes for /api/goals
 router.get('/',getGoals)
-router.post('/',setGoal)
-/* Instead of above 2
-router.route('/').get(getGoals).post(setGoal)*/
+router.post('/',setGoal)*/
 
-router.put('/:id',updateGoal);
-router.delete('/:id',deleteGoal)
-/* Instead of above 2
-router.route('/:id').delete(deleteGoal).put(updateGoal)*/
+/* Instead of above 2*/
+router.route('/').get(protect,getGoals).post(protect,setGoal)
+
+/*router.put('/:id',updateGoal);
+router.delete('/:id',deleteGoal)*/
+
+/* Instead of above 2*/
+router.route('/:id').delete(protect,deleteGoal).put(protect,updateGoal)
 
 module.exports=router
