0b1bcaba18086984b8ff419b60c7d644e30118e0
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2022-03-01 09:39:24 +0530

    Pages and routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>My app</h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Dashboard/>}></Route>
+            <Route path="/register" element={<Register/>}></Route>
+            <Route path="/login" element={<Login/>}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
